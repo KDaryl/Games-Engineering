7ee6cbe27802038ec98a3c6d0753c0cabe59014d
KDaryl <c00207452@itcarlow.ie>
2018-11-22 09:43:03 +0000

    Added flow graph and complexity

--- Practical 4 - Simple Assert/Test Framework/TestMain.cpp
+++ Practical 4 - Simple Assert/Test Framework/TestMain.cpp
@@ -4,13 +4,18 @@
 void main()
 {
 	Test test; //Used for assertions
-	Game game;
+	//Game game;
 
 	game.run();
 
+	//Testing the range
 	test.range(game.values());
 	test.repeated(game.values());
 	test.size(game.values());
+
+	//Testing fake data, should fail here as first line has only 5 values and the second is empty
+	test.size({1 ,1, 1, 1, 1,1});
+	test.size({ });
 
 	std::cout << "All tests passed" << std::endl;
 
